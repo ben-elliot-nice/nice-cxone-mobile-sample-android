58913dc2f40a2d26789b1ac4bac611b0e0396960
Ben Elliot <benjiboi214@gmail.com>
2024-03-18 13:13:35 +1100

    back out changes to core resrouces

--- app/src/main/java/com/nice/cxonechat/sample/data/ChatThreadHandlerKt.kt
+++ app/src/main/java/com/nice/cxonechat/sample/data/ChatThreadHandlerKt.kt
@@ -1,6 +1,5 @@
 package com.nice.cxonechat.sample.data
 
-import android.util.Log
 import com.nice.cxonechat.ChatThreadHandler
 import com.nice.cxonechat.thread.ChatThread
 import kotlinx.coroutines.channels.awaitClose
@@ -9,7 +8,6 @@ import kotlinx.coroutines.flow.callbackFlow
 
 internal val ChatThreadHandler.flow: Flow<ChatThread>
     get() = callbackFlow {
-        Log.e("ChatThreadHandlerFlow", "Getting most up to date thread")
         val cancellable = get(::trySend)
         send(get())
         refresh()
